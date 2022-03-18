4b2709edc9c314a6aad75c8a5399ac997157c84c
atisheks1 <84221898+atisheksingh@users.noreply.github.com>
2022-03-18 18:42:34 +0530

    sawpping done

---
+++ src/constants/chains.js
@@ -0,0 +1,30 @@
+export const networks = [3,4,5,42,123, 1337, 444800, 444900,80001]
+// export const networks = [80001]
+
+export const ChainId = {
+  // MAINNET: 1,
+  // ROPSTEN: 3,
+  // RINKEBY: 4,
+  // GÖRLI: 5,
+  // KOVAN: 42,
+  // DEVNET: 444800,
+  // AUTONITY: 444900,
+  // PARASTATE: 123,
+  // GANCHE: 1337,
+  POLYGON: 80001
+};
+
+export const routerAddress = new Map();
+// routerAddress.set(ChainId.MAINNET, "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D");
+// routerAddress.set(ChainId.ROPSTEN, "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D");
+// //routerAddress.set(ChainId.RINKEBY, "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D");
+// routerAddress.set(ChainId.RINKEBY, "0xF66c4c1d8e3eB69148483c0eb6dc225043579D49");
+// routerAddress.set(ChainId.GÖRLI, "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D");
+// routerAddress.set(ChainId.KOVAN, "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D");
+// routerAddress.set(ChainId.DEVNET, "0x04e555283D37aE85F6eB311fe2578F3B3f3dFc52");
+// routerAddress.set(ChainId.AUTONITY, "0x04e555283D37aE85F6eB311fe2578F3B3f3dFc52");
+// routerAddress.set(ChainId.PARASTATE, "0x07a1905D44feeA439ceFAabd11a63bEf684abE11");
+// routerAddress.set(ChainId.GANCHE, "0x0F44AC51198D8F99847db6C431448dBC673428f1");
+routerAddress.set(ChainId.POLYGON, "0xfBE48bF5b3e77625bF46C9ca195F5cAFe5601357");
+
+
